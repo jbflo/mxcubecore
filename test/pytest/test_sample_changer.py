0e2573aefa54532af26b079decfb8a1a25f74ae4
JB <jbflorial2@gmail.com>
2020-04-02 18:27:15 +0100

    Auto stash before checking out "HEAD"

--- test/pytest/test_sample_changer.py
+++ test/pytest/test_sample_changer.py
@@ -1,3 +1,7 @@
+from HardwareRepository import HardwareRepository as HWR
+from HardwareRepository.HardwareObjects.abstract import AbstractSampleChanger
+
+
 def test_sample_change_init(beamline):
     assert (
         not beamline.sample_changer is None
@@ -15,7 +19,10 @@ def test_sample_change_select(beamline):
   pass
 
 def test_sample_change_abort(beamline):
-  pass
+  assert not None  beamline.sample_changer.task_proc, "task_proc is None"
+  target = 12.7
+  beamline.energy.set_value(target)
+  assert beamline.energy.get_value() == target
 
 def test_sample_change_get_state(beamline):
   pass
@@ -24,7 +31,12 @@ def test_sample_change_get_status(beamline):
   pass
 
 def test_sample_change_has_loaded_sample(beamline):
-  pass
+      assert (
+        not beamline.sample_changer.has_loaded_sample is None
+    ), "Sample changer hardware objects is None (not initialized)"
 
 def test_sample_change_get_loaded_sample(beamline):
-  pass
+    loaded_sample = len(beamline.sample_changer.get_loaded_sample)
+      assert (
+        loaded_sample >= 0
+    ), "Sample changer get_loaded_sample must be >= 0 but is None (not initialized)"
